31e95a707fd2ec9a7d4a3384d736f733039c45c9
Uttam Biswas <biswasuttam0911@gmail.com>
2024-08-26 00:29:37 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000";
+  const url="https://food-ordering-backend-qbma.onrender.com";
   const [token,setToken]=useState("");
   const [food_list,setFood_list]=useState([])
 
